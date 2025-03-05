e76847579040936febf7356e0fc7e34a1e421e63
Hii-99 <hiiluann994q@gmail.com>
2025-03-05 22:50:17 +0800

    update config location; adding env variable

--- script/HRD_calculation/bam2seqz_parallel.2.sh
+++ script/HRD_calculation/bam2seqz_parallel.2.sh
@@ -3,7 +3,9 @@
 # [ ] get base_dir, get filepath
 # [ ] script
 
-source /home/hiiluann99/playground/HRD/HRD_pipeline/preprocessing/config.sh
+shopt -s expand_aliases
+
+source $HRD_PIPELINE_CONFIG
 
 CHR="chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY"
 
