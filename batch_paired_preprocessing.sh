810ed3903c58927368b04897b25848d693f2db40
Hii-99 <hiiluan994q@gmail.com>
2025-03-31 13:28:51 +0800

    batch paired preprocessing

---
+++ batch_paired_preprocessing.sh
@@ -0,0 +1,15 @@
+shopt -s expand_aliases
+
+source $HRD_PIPELINE_CONFIG
+
+shopt -s expand_aliases
+
+FILE_LIST=$1
+INPUT_DIR=$2
+OUT_DIR=$3
+
+while read -r file; do
+    [[ -z "$file" ]] && continue  # Skip empty lines
+    echo "Downloading: $file"
+    bash $HRD_DIR/paired_preprocessing.sh $INPUT_DIR/$file $OUT_DIR $OUT_DIR
+done < "$FILE_LIST" 
